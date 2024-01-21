aefcb1655ef40bd5de1b039e2a37311478c6a076
Lu Pinheiro <145330939+Lupinheiroi2t@users.noreply.github.com>
2024-01-22 00:15:28 +0100

    Add files via upload

--- CalculadorRutas/models/route_calculator.py
+++ CalculadorRutas/models/route_calculator.py
@@ -1,7 +1,7 @@
 # © 2023 Lu Pinheiro (<https://github.com/Lucpinheiro>)
 # License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class RouteCalculator(models.Model):
@@ -11,4 +11,13 @@ class RouteCalculator(models.Model):
     name = fields.Char(related='connection_id.name')
     connection_id = fields.Many2one("connection")
     type_id = fields.Many2one("connection.type")
+
  
+    @api.model
+    def create(self, values):
+        # Llama al método create de la clase base para realizar la creación del registro
+        record = super(RouteCalculator, self).create(values)
+        # Muestra un mensaje de "Feliz viaje"
+        self.env.user.notify_warning(message="¡Feliz viaje!")
+
+        return record
