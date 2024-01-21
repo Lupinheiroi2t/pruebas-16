85fd9652ecf55d23dc001a686461a66257e77ff6
Lu Pinheiro <145330939+Lupinheiroi2t@users.noreply.github.com>
2024-01-21 21:27:02 +0100

    Add files via upload

--- CalculadorRutas/models/connection.py
+++ CalculadorRutas/models/connection.py
@@ -30,20 +30,10 @@ class Connection(models.Model):
             if  connection.origin.name == 'Malta' and connection.destination.name == 'Valencia':
                 raise ValidationError('No existe ruta válida entre Malta y Valencia.')
 
-                 
-         
-    # @api.constrains('origin', 'destination')
-    # def _compute_connections(self):
-    #     for connection in self:
-    #         name = f"{connection.origin.name} - {connection.destination.name}"
-    #         connection.name = name
-    #         if not connection.origin or not connection.destination:
-    #             raise ValidationError("Debe especificar tanto la ciudad de origen como la de destino.")
-    #         if (
-    #             connection.origin.name == 'Paris' and
-    #             connection.destination.name == 'Barcelona'
-    #         ):
-    #             raise ValidationError('No existe una válida entre Paris y Barcelona.')
-                 
-                 
-                 
+
+class ConnectionType(models.Model):
+    _name = "connection.type"
+    _description = "Connection Type"
+
+    name = fields.Char()
+    type = fields.Char()
