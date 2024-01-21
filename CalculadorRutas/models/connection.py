7ef11d3634b4657f33be738c612b6ae579ac7ee0
Lu Pinheiro <145330939+Lupinheiroi2t@users.noreply.github.com>
2024-01-21 20:27:21 +0100

    Add files via upload

--- CalculadorRutas/models/connection.py
+++ CalculadorRutas/models/connection.py
@@ -21,17 +21,18 @@ class Connection(models.Model):
             connection.name = name
             if not connection.origin or not connection.destination:
                 raise ValidationError("Debe especificar tanto la ciudad de origen como la de destino.")
-            if (
-                        connection.origin.name.lower() == 'paris' and
-                        connection.destination.name.lower() != 'barcelona'
-                    ) or (
-                        connection.origin.name.lower() != 'paris' and
-                        connection.destination.name.lower() == 'barcelona'
-                    ):
-                        raise ValidationError("Las conexiones deben ser entre 'Paris' y 'Barcelona'.")
+            valor_buscado = 'Paris'  
+            destino_encontrado = self.env['city'].search([('name', '=', valor_buscado)])
 
-
-
+        if destino_encontrado:
+            # Hacer algo con el resultado, por ejemplo, imprimir el ID
+            print("ID del destino encontrado:", destino_encontrado.id)
+        else:
+            print("No se encontró ningún destino con el valor deseado.")
+                 
+                 
+                 
+        
 
     # @api.constrains('origin', 'destination')
     # def _compute_destination(self):
@@ -41,15 +42,7 @@ class Connection(models.Model):
     #         if  connection.origin == "paris" and  connection.destination == "barcelona":
     #             raise ValidationError("No existe ruta válida para estas ciudades'.")
 
-    api.constrains('origin', 'destination')
-    def _check_campos_relacionados_validos(self):
-            for connection in self:
-  
-                ciudad_origen_name  = connection.origin.name.lower()
-                ciudad_origen_name  = connection.destination.name.lower()
 
-            if ciudad_origen_name == 'paris' and ciudad_origen_name == 'barcelona':
-                raise ValidationError("La ciudad de origen no puede ser 'Paris' cuando la ciudad de destino es 'Barcelona'.")
     
     # city_connection_ids = fields.Selection(
     #     selection=[
