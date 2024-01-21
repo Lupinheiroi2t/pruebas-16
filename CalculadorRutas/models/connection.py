55ecf93b0ba090d476e555b97c20d4d047b10798
Lu Pinheiro <145330939+Lupinheiroi2t@users.noreply.github.com>
2024-01-21 19:37:10 +0100

    Add files via upload

--- CalculadorRutas/models/connection.py
+++ CalculadorRutas/models/connection.py
@@ -10,29 +10,51 @@ class Connection(models.Model):
 
     name = fields.Char(string="Conexión", compute="_compute_connections", store=True)
     type = fields.Char()
-    city_connection_ids = fields.Selection(
-        selection=[
-            ("paris", "Paris"),
-            ("barcelona", "Barcelona"),
-            ("madrid", "Madrid"),
-            ("roma", "Roma"),
-            ("valencia", "Valencia"),
-            ("malta", "malta"),
-        ],)
-    
-    origin = fields.Selection(related='city_connection_ids.origin')
-    destination = fields.Selection(related='city_connection_ids.destination')
+    origin = fields.Many2one("city")
+    destination = fields.Many2one("city")
 
-    @api.constrains('origin', 'destination')
+
+            
+    @api.deprends('origin', 'destination')
     def _compute_connections(self):
         for connection in self:
-            if not connection.origin or not connection.destination:
-                raise ValidationError("Debe especificar tanto la ciudad de origen como la de destino.")
-            if  connection.origin == "paris" and  connection.destination == "barcelona":
-                raise ValidationError("No existe ruta válida para estas ciudades'.")
-            
             name = f"{connection.origin.name} - {connection.destination.name}"
             connection.name = name
 
+    api.constrains('origin', 'destination')
+    def _check_campos_relacionados_validos(self):
+            for connection in self:
+                ciudad_origen_name  = connection.ciudad_origen.name.lower()
+                ciudad_origen_name  = connection.ciudad_destino.name.lower()
 
-       
+            if ciudad_origen_name == 'paris' and ciudad_origen_name  == 'barcelona':
+                raise ValidationError("La ciudad de origen no puede ser 'Paris' cuando la ciudad de destino es 'Barcelona'.")
+    
+    # city_connection_ids = fields.Selection(
+    #     selection=[
+    #         ("paris", "Paris"),
+    #         ("barcelona", "Barcelona"),
+    #         ("madrid", "Madrid"),
+    #         ("roma", "Roma"),
+    #         ("valencia", "Valencia"),
+    #         ("malta", "malta"),
+    #     ],)
+    
+    # origin = fields.Selection(related='city_connection_ids.origin')
+    # destination = fields.Selection(compute="_compute_destination", related='city_connection_ids.destination')
+
+    # @api.constrains('origin', 'destination')
+    # def _compute_destination(self):
+    #     for connection in self:
+    #         if not connection.origin or not connection.destination:
+    #             raise ValidationError("Debe especificar tanto la ciudad de origen como la de destino.")
+    #         if  connection.origin == "paris" and  connection.destination == "barcelona":
+    #             raise ValidationError("No existe ruta válida para estas ciudades'.")
+            
+            
+    # @api.deprends('origin', 'destination')
+    # def _compute_connections(self):
+    #     for connection in self:
+    #         name = f"{connection.origin.name} - {connection.destination.name}"
+    #         connection.name = name
+
