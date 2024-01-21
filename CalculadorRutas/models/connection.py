1bba22c374f86d5418667c80e21624190b72293f
Lu Pinheiro <145330939+Lupinheiroi2t@users.noreply.github.com>
2024-01-21 19:12:48 +0100

    Add files via upload

--- CalculadorRutas/models/connection.py
+++ CalculadorRutas/models/connection.py
@@ -10,17 +10,29 @@ class Connection(models.Model):
 
     name = fields.Char(string="Conexión", compute="_compute_connections", store=True)
     type = fields.Char()
-    origin = fields.Many2one("city")
-    destination = fields.Many2one("city")
-
+    city_connection_ids = fields.Selection(
+        selection=[
+            ("paris", "Paris"),
+            ("barcelona", "Barcelona"),
+            ("madrid", "Madrid"),
+            ("roma", "Roma"),
+            ("valencia", "Valencia"),
+            ("malta", "malta"),
+        ],)
+    
+    origin = fields.Selection(related='city_connection_ids.origin')
+    destination = fields.Selection(related='city_connection_ids.destination')
 
     @api.constrains('origin', 'destination')
     def _compute_connections(self):
         for connection in self:
             if not connection.origin or not connection.destination:
                 raise ValidationError("Debe especificar tanto la ciudad de origen como la de destino.")
-            if connection.origin == "Barcelona" and connection.destination == "Paris":
+            if  connection.city_connection_ids in ["paris", "barcelona"]:
                 raise ValidationError("No existe ruta válida para estas ciudades'.")
             
             name = f"{connection.origin.name} - {connection.destination.name}"
             connection.name = name
+
+
+       
