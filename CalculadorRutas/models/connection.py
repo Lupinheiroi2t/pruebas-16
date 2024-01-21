16f8549f4331e6ea67f1cfb062e9684fce66afd0
Lu Pinheiro <145330939+Lupinheiroi2t@users.noreply.github.com>
2024-01-21 18:49:31 +0100

    Add files via upload

--- CalculadorRutas/models/connection.py
+++ CalculadorRutas/models/connection.py
@@ -2,7 +2,7 @@
 # License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
 from odoo import fields, models, api 
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
 
 class Connection(models.Model):
     _name = "connection"
@@ -14,18 +14,15 @@ class Connection(models.Model):
     destination = fields.Many2one("city")
 
 
-    @api.depends('origin', 'destination')
+    @api.constrains('origin', 'destination')
     def _compute_connections(self):
         for connection in self:
-            try:
-                if not connection.origin or not connection.destination:
-                    raise ValidationError("Debe especificar tanto la ciudad de origen como la de destino.")
-                if connection.origin.name == "Barcelona" and connection.destination.name == "Paris":
-                    raise ValidationError("No existe ruta válida para estas ciudades'.")
-                
-                name = f"{connection.origin.name} - {connection.destination.name}"
-                connection.name = name
+            if not connection.origin or not connection.destination:
+                raise ValidationError("Debe especificar tanto la ciudad de origen como la de destino.")
+            if connection.origin.name == "Barcelona" and connection.destination.name == "Paris":
+                raise ValidationError("No existe ruta válida para estas ciudades'.")
+            
+            name = f"{connection.origin.name} - {connection.destination.name}"
+            connection.name = name
 
-            except ValidationError as e:
-                connection.name = False
 
