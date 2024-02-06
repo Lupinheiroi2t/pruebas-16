3ab27adeb550f7f58f07d0c54d85996ea131797b
Lu Pinheiro <145330939+Lupinheiroi2t@users.noreply.github.com>
2024-02-06 13:17:09 +0100

    Add files via upload

---
+++ i2t_negative_amounts_credit_note_reports-copia/models/purchase_order.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    move_type = fields.Selection(related='invoice_ids.move_type')
+    
+  
